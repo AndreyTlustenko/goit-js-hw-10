7dba4d4d873079f49e263a41f2d83a2fa5730d8a
AndreyTlustenko <andreyt2018@ukr.net>
2022-11-13 21:48:01 +0200

    1

---
+++ src/fetchCountries.js
@@ -0,0 +1,24 @@
+export {fetchCountries};
+import Notiflix from 'notiflix';
+
+
+function fetchCountries(name){
+   return fetch (`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`)
+    .then((response) => {
+        if (!response.ok){
+            throw new Error (response.status);
+        }
+        return response.json();
+    }).then(data => console.log(data))
+    .catch (error =>{(Notiflix.Notify.failure("Oops, there is no country with that name"))})
+}
+fetchCountries('ukraine');
+
+// ())
+
+// fetchCountries(name) {
+//     return fetch('https://restcountries.com/v3.1/name/{name}')
+//       .then(response => response.json())
+//       .then(data => console.log(`${name}`, name));
+//   }
+//   fetchCountries('ukraine');
--- src/index.js
+++ src/index.js
@@ -1,3 +1,54 @@
 import './css/styles.css';
+import debounce from 'lodash.debounce';
+// import './fetchCountries';
+import {fetchCountries} from "./fetchCountries";
+fetchCountries('ukraine');
 
 const DEBOUNCE_DELAY = 300;
+
+const input = document.querySelector('#search-box');
+const countryList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
+
+input.addEventListener('input', debounce(search,DEBOUNCE_DELAY))
+
+function search(e){
+    e.preventDefault();
+    const inputData = input.value.trim();
+    fetchCountries(inputData).then(rendercountryList).catch(console.log)
+}
+
+function clear (){
+    countryList.innerHTML = '';
+    countryInfo.innerHTML = '';
+}
+
+function rendercountryList(responseAPI){
+    console.log(responseAPI);
+    clear();
+    if (responseAPI.lenght > 10){
+        Notiflix.Notify.info('Too many matches found. Please enter a more specific name.')
+    }else if (responseAPI.lenght === 1){
+        countryInfo.innerHTML = renderCountryInfo(responseAPI[0]);
+    }else {
+        const renderListCountry = responseAPI.map(country => renderCountriesList(country)).join('');
+        countryList.insertAdjacentHTML('beforeend',renderListCountry);
+    }
+}
+
+function renderCountriesList({flag, name}){
+    return `<li class="country-listInfo">
+            <img class="country-flag" src="${flag.svg}"/>
+            <h2 class="country-listName">${name.official}</h2>
+            </li>`;
+}
+function renderCountryInfo({name, flag, capital, population, languages}){
+    return `<li class="country-listInfo">
+    <img class="country-flagInfo" src="${flag.svg}"/>
+    <h2 class="country-listName">${name.official}</h2>
+
+    <p><b> capital:</b> ${capital}</p>
+    <p><b> population:</b> ${population}</p>
+    <p><b> languages:</b> ${Object.values(languages)}</p>
+    </li>`;
+}
